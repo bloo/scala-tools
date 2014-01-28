35003fc22c2e045933926ed2e060e261cae4555b
Blue Thomas <blue.thomas@gmail.com>
2014-01-27 19:47:23 -0500

    need to lazily init properties

--- common/src/main/scala/b/email/SessionFactory.scala
+++ common/src/main/scala/b/email/SessionFactory.scala
@@ -10,12 +10,17 @@ trait SimpleSmtpSessionFactory extends EmailSessionFactory {
 
 	val host: String
 	val port = 25
-
-    val props = new java.util.Properties
-    props.put("mail.smtp.host", host)
-    props.put("mail.smtp.port", port.asInstanceOf[java.lang.Integer])
     
-    override def session: Session = Session.getInstance(props)
+	lazy val props = {
+	    val props = new java.util.Properties
+	    props.put("mail.smtp.host", host)
+	    props.put("mail.smtp.port", port.asInstanceOf[java.lang.Integer])
+	    props
+	}
+	
+    override def session: Session = {
+	    Session.getInstance(props)
+	}
 }
 
 trait SslSmtpSessionFactory	extends EmailSessionFactory {
@@ -24,18 +29,23 @@ trait SslSmtpSessionFactory	extends EmailSessionFactory {
 	val password: String
     val host: String
 	val port = 465
-  
-    val props = new java.util.Properties
-    props.put("mail.smtp.host", host)
-    props.put("mail.smtp.port", port.asInstanceOf[java.lang.Integer])
-    props.put("mail.smtp.port", port.asInstanceOf[java.lang.Integer])
-    props.put("mail.smtp.socketFactory.port", port.asInstanceOf[java.lang.Integer])
-    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
-    props.put("mail.smtp.auth", "true")
 
-    override def session: Session = Session.getDefaultInstance(props, new javax.mail.Authenticator {
-        override def getPasswordAuthentication = {
-            new javax.mail.PasswordAuthentication(user, password)
-        }
-    })
+	lazy val props = {
+	    val props = new java.util.Properties
+	    props.put("mail.smtp.host", host)
+	    props.put("mail.smtp.port", port.asInstanceOf[java.lang.Integer])
+	    props.put("mail.smtp.port", port.asInstanceOf[java.lang.Integer])
+	    props.put("mail.smtp.socketFactory.port", port.asInstanceOf[java.lang.Integer])
+	    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+	    props.put("mail.smtp.auth", "true")	  
+	    props
+	}
+	
+    override def session: Session = {	  
+		Session.getDefaultInstance(props, new javax.mail.Authenticator {
+	        override def getPasswordAuthentication = {
+	            new javax.mail.PasswordAuthentication(user, password)
+	        }
+	    })
+	}
 }
