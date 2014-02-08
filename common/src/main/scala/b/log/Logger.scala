70441d3e84155d467376182fdd33e0853edbf350
Blue Thomas <blue.thomas@gmail.com>
2014-02-08 17:26:46 -0500

    simpler logging methods

--- common/src/main/scala/b/log/Logger.scala
+++ common/src/main/scala/b/log/Logger.scala
@@ -2,5 +2,10 @@ package b.log
 
 trait Logger {
 	val logger = grizzled.slf4j.Logger(getClass)
-	val log = logger
+	def log(msg: String) = logger info msg
+	def info(msg: String) = logger info msg
+	def debug(msg: String) = logger debug msg
+	def warn(msg: String) = logger warn  msg
+	def error(msg: String) = logger error msg
+	def error(msg: String, t: Throwable) = logger error(msg,t)
 }
